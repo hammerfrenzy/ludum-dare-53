a27351a6aefdfd7afab83fa44c559aef69628818
Wyatt Beavers <hammerfrenzy@gmail.com>
2023-05-01 19:19:20 -0500

    Remove duplicate SetBool

--- Assets/Scripts/Character/RatControllerBehavior.cs
+++ Assets/Scripts/Character/RatControllerBehavior.cs
@@ -152,8 +152,6 @@ public class RatControllerBehavior : MonoBehaviour
         isBeingControlled = giveControl;
         selectionRenderer.enabled = giveControl;
 
-        animator.SetBool("isInteracting", isInteracting);
-
         if (giveControl && voiceManager != null && shouldQuip) 
         {
             voiceManager.PlayQuip(isRico, isHorace, isNixie);
